0c329ef8f78fc6188ebd01192d732cf44788fa7a
gmendes0 <gmendes230@gmail.com>
2020-03-25 13:28:56 -0300

    refactor: move methods to controllers directory

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,10 @@
 const express = require("express");
-const crypto = require("crypto");
-const connection = require("./database/connection");
 
 const route = express.Router();
 
-route.get("/ongs", async (request, response) => {
-  const ongs = await connection("ongs").select("*");
+const OngController = require("./controllers/OngController");
 
-  return response.json(ongs);
-});
-
-route.post("/ongs", async (request, response) => {
-  const { name, email, whatsapp, city, uf } = request.body;
-  const id = crypto.randomBytes(4).toString("HEX");
-
-  await connection("ongs").insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf
-  });
-
-  response.json({ id });
-});
+route.get("/ongs", OngController.index);
+route.post("/ongs", OngController.store);
 
 module.exports = route;
