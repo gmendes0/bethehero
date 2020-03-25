8a53c8bcdef6fd4a9caebaad2e71daa700755d6e
gmendes0 <gmendes230@gmail.com>
2020-03-25 13:23:51 -0300

    feat(ongs): added store and index routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,29 @@
 const express = require("express");
+const crypto = require("crypto");
+const connection = require("./database/connection");
 
 const route = express.Router();
 
-route.get("/", (request, response) => response.send("Ok"));
+route.get("/ongs", async (request, response) => {
+  const ongs = await connection("ongs").select("*");
+
+  return response.json(ongs);
+});
+
+route.post("/ongs", async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+  const id = crypto.randomBytes(4).toString("HEX");
+
+  await connection("ongs").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+  response.json({ id });
+});
 
 module.exports = route;
