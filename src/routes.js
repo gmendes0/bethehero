0c687bc34a188bba095288ba20cabe6a2927b1c6
mendesg0 <gabriel@prestige.com.br>
2020-03-26 15:26:13 -0300

    feat: added incidents and profiles

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,14 @@ const express = require("express");
 const route = express.Router();
 
 const OngController = require("./controllers/OngController");
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 route.get("/ongs", OngController.index);
 route.post("/ongs", OngController.store);
+route.get("/profiles", ProfileController.index);
+route.get("/incidents", IncidentController.index);
+route.post("/incidents", IncidentController.store);
+route.delete("/incidents/:id", IncidentController.destroy);
 
 module.exports = route;
